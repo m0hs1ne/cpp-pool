39a196358639418fa87f65140d721977ced43166
m0hs1ne <mel-hada@student.1337.ma>
2022-08-04 14:30:57 +0100

    Moar brainz!

--- Module 01/ex01/zombieHorde.cpp
+++ Module 01/ex01/zombieHorde.cpp
@@ -2,6 +2,8 @@
 
 Zombie *zombieHorde(int n,std::string name)
 {
+    if (n < 0)
+        return 0;
     Zombie *zombie = new Zombie[n];
     for (int i = 0; i < n; i++)
         zombie[i].setName(name);
