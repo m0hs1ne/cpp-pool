8855e67d3709162172b3010d4ed117ef35a3888b
m0hs1ne <mel-hada@student.1337.ma>
2022-12-05 16:52:32 +0100

    Identify real type

--- Module 06/ex02/main.cpp
+++ Module 06/ex02/main.cpp
@@ -18,12 +18,19 @@ class C : public Base
 
 Base *generate()
 {
-    srand(time(NULL));
+    std::srand(time(NULL));
     int r = std::rand() % 3;
     if (r == 0)
+    {
+        std::cout << "A" << std::endl;
         return new A();
+    }
     if (r == 1)
+    {
+        std::cout << "B" << std::endl;
         return new B();
+    }
+    std::cout << "C" << std::endl;
     return new C();
 }
 
@@ -35,8 +42,6 @@ void identify(Base *p)
         std::cout << "B" << std::endl;
     else if (dynamic_cast<C *>(p))
         std::cout << "C" << std::endl;
-    else
-        std::cout << "Unknown type" << std::endl;
 }
 
 void identify(Base &p)
@@ -48,6 +53,7 @@ int main()
 {
     Base *p = generate();
     identify(p);
+    identify(*p);
     delete p;
     return 0;
 }
