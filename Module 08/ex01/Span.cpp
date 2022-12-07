b73eaefcc7f2b5eb1cab42442923cb73d33481ca
m0hs1ne <mel-hada@student.1337.ma>
2022-12-07 16:30:30 +0100

    Span

---
+++ Module 08/ex01/Span.cpp
@@ -0,0 +1,65 @@
+#include "Span.hpp"
+
+Span::Span(unsigned int n) : _n(n)
+{
+}
+
+Span::Span(Span const &src)
+{
+    *this = src;
+}
+
+Span::~Span()
+{
+}
+
+Span &Span::operator=(Span const &rhs)
+{
+    if (this != &rhs)
+    {
+        _v = rhs._v;
+        _n = rhs._n;
+    }
+    return *this;
+}
+
+void Span::addNumber(int n)
+{
+    if (_v.size() == _n)
+        throw FullException();
+    _v.push_back(n);
+}
+
+int Span::shortestSpan()
+{
+    if (_v.size() < 2)
+        throw NoSpanException();
+    std::vector<int> v(_v);
+    std::sort(v.begin(), v.end());
+    int min = v[1] - v[0];
+    for (unsigned int i = 2; i < v.size(); i++)
+    {
+        if (v[i] - v[i - 1] < min)
+            min = v[i] - v[i - 1];
+    }
+    return min;
+}
+
+int Span::longestSpan()
+{
+    if (_v.size() < 2)
+        throw NoSpanException();
+    std::vector<int> v(_v);
+    std::sort(v.begin(), v.end());
+    return v[v.size() - 1] - v[0];
+}
+
+const char *Span::FullException::what() const throw()
+{
+    return "Span is full";
+}
+
+const char *Span::NoSpanException::what() const throw()
+{
+    return "Span has no span";
+}
