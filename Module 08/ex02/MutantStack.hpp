61786b87d9bb2a72665938db537f82be4cf0b957
m0hs1ne <mel-hada@student.1337.ma>
2022-12-07 17:46:30 +0100

    Mutated abomination

---
+++ Module 08/ex02/MutantStack.hpp
@@ -0,0 +1,34 @@
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+#include <stack>
+#include <iostream>
+#include <deque>
+
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+public:
+    MutantStack() : std::stack<T>() {}
+    MutantStack(MutantStack const &src) : std::stack<T>(src) {}
+    ~MutantStack() {}
+    MutantStack &operator=(MutantStack const &rhs)
+    {
+        if (this != &rhs)
+            std::stack<T>::operator=(rhs);
+        return *this;
+    }
+
+    typedef typename std::deque<T>::iterator iterator;
+
+    iterator begin()
+    {
+        return std::stack<T>::c.begin();
+    }
+    iterator end()
+    {
+        return std::stack<T>::c.end();
+    }
+};
+
+#endif
